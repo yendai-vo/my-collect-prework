36ef53f6f4789d08acad41bdc6ff028a0e9d9475
Cindy Vo <yendai.vo@hotmail.com>
2018-07-13 16:23:24 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(array)
+ item = []
+     i = 0
+     while i < array.length
+       item.push yield array[i]
+       i += 1
+     end
+   item
+ end
 
-
+# def my_collect(array)
+#   i = 0
+#   collect = []
+#   while i < array.length
+#     collect.push yield(array[i])
+#     i+=1
+#   end
+#   collect
+# end
